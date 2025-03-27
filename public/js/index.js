cd15e2aed36e8f3cfb6d6000dc8256d14bcce365
youzhen <yl301114@gmail.com>
2025-03-27 15:14:59 +0000

    修1

--- public/js/index.js
+++ public/js/index.js
@@ -10,3 +10,11 @@ $(".nav_menu").click(function() {
 $(".account_pic").click(function() {
     $(".account_nav").slideToggle(300);
 })
+
+$(".modal_bg").click(function() {
+    $(this).parents(".modal").fadeOut(300);
+})
+
+$(".btn-func").click(function() {
+    $("#modal-edit").fadeIn(300);
+})
