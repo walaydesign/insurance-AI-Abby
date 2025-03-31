e921c3e753780cacda52f7093e58cc3ca1ea26ce
youzhen <yl301114@gmail.com>
2025-03-31 23:22:01 +0100

    修2

--- public/js/index.js
+++ public/js/index.js
@@ -16,9 +16,29 @@ $(".modal_bg").click(function() {
 })
 
 $(".btn-func").click(function() {
-    $("#modal-edit").fadeIn(300);
+    // $("#modal-edit").fadeIn(300);
+    $(this).find(".btn-func_list").fadeToggle(300);
 })
+
+$(document).click(function (event) {
+    var btnFunc = $(".btn-func");
+    if (!btnFunc.is(event.target) && btnFunc.has(event.target).length === 0) {
+        $(".btn-func_list").fadeOut(300);
+    }
+});
 
 $(".btn-loading").click(function() {
     $(".loading").fadeIn(300);
 })
+
+$(window).on("resize scroll",function(){
+    chatBoxHeight();
+})
+chatBoxHeight();
+function chatBoxHeight() {
+    // if(window().width() < 768) {
+        let height = parseInt($(".chat_bottom_inner").css("height"));
+        let chatPadding = height + 20;
+        $(".chat_inner").css("padding-bottom", chatPadding);
+    // }
+}
